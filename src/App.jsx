1ba7c29c2312f3474e27a65030f73a8f7e277711
faiyaj <faiyajz007@gmail.com>
2022-03-09 10:09:33 +0600

    bootstrap added, navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./Navbar";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
@@ -7,13 +9,17 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 
 const App = () => {
     return (
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/service' element={<Service/>} />
-            <Route path='/contact' element={<Contact/>} />
-            <Route path='/about' element={<About/>} />
-            <Route path='*' element={<Navigate replace to="/" />} />
-        </Routes>
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/service' element={<Service/>} />
+                <Route path='/contact' element={<Contact/>} />
+                <Route path='/about' element={<About/>} />
+                <Route path='*' element={<Navigate replace to="/" />} />
+            </Routes>
+        </>
+
 
     )
 }
