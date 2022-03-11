309661de5ad94dcf8ea27419f8b8d105cf032e43
faiyaj <faiyajz007@gmail.com>
2022-03-11 11:50:27 +0600

    Home page header added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import web from "../src/images/home-page-animation.gif";
+import {NavLink} from "react-router-dom";
 
 const Home = () => {
     return (
         <>
-            <h1>Home Alhamdulillah !</h1>
+            <section id="header" className="d-flex align-items-center">
+                <div className="container-fluid nav_bg">
+                    <div className="row">
+                        <div className="col-10 mx-auto">
+                            <div className="row">
+                                <div className="col-md-6 pt-2 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                                    <h1>Grow your business with <strong className="brand-name"> MyCompanyName</strong>
+                                    </h1>
+                                    <h2 className="my-3">We are the team of talented Software Engineers</h2>
+                                    <div className="mt-3">
+                                        <NavLink to="/service" className="btn-get-started">Get Started</NavLink>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 order-1 order-lg-2 header-img">
+                                    <img src={web} className="img-fluid animated" alt="home-img"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
